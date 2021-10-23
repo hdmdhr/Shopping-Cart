8dc3496e17066cf104bf561e03d90aad0205261f
Daniel Hu <hdmdhr@gmail.com>
2021-10-22 19:46:19 -0600

    Rename `ViewConotroller` to `CountViewController`, set organization name.

--- Shopping Cart/CountViewController.swift
+++ Shopping Cart/CountViewController.swift
@@ -1,13 +1,14 @@
 //
-//  ViewController.swift
+//  CountViewController.swift
 //  Shopping Cart
 //
 //  Created by 胡洞明 on 2021-10-22.
+//  Copyright © 2021 Vog App Developers. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class CountViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
