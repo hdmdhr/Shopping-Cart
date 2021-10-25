18b72097fe1972342fc5eb1e7577db6234198ce1
Daniel Hu <hdmdhr@gmail.com>
2021-10-24 19:47:55 -0600

    Add `SaveLoadProductManager`

---
+++ Shopping Cart/Manager/SaveLoadProductManager.swift
@@ -0,0 +1,21 @@
+//
+//  SaveLoadProductManager.swift
+//  Shopping Cart
+//
+//  Created by 胡洞明 on 2021-10-24.
+//  Copyright © 2021 Vog App Developers. All rights reserved.
+//
+
+import Foundation
+
+class SaveLoadProductManager {
+    
+    func saveNumberForProduct(_ product: String, number: Int) {
+        UserDefaults.standard.setValue(number, forKey: product)
+    }
+    
+    func loadNumberForProduct(_ product: String) -> Int {
+        UserDefaults.standard.value(forKey: product) as? Int ?? 0
+    }
+    
+}
