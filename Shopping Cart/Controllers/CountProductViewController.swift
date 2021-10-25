e7c1e472b14b3c7ebb8165b46b24a9eea66f2541
Daniel Hu <hdmdhr@gmail.com>
2021-10-24 19:42:40 -0600

    Make `CountProductViewController` configurable

---
+++ Shopping Cart/Controllers/CountProductViewController.swift
@@ -0,0 +1,67 @@
+//
+//  CountProductViewController.swift
+//  Shopping Cart
+//
+//  Created by 胡洞明 on 2021-10-24.
+//  Copyright © 2021 Vog App Developers. All rights reserved.
+//
+
+import UIKit
+
+class CountProductViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var productImageView: UIImageView!
+    @IBOutlet private weak var productCountLabel: UILabel!
+    @IBOutlet private weak var stepper: UIStepper!
+    
+    
+    // MARK: - Vars
+    
+    private(set) var productName: String!
+    private(set) var increment: Int!
+    private(set) var maxPurchaseNumber: Int!
+    
+    private var productNumber: Int { stepper.value.toRoundedInt }
+    
+    func configure(productName: String, increment: Int, maxPurchaseNumber: Int) {
+        self.productName = productName
+        self.increment = increment
+        self.maxPurchaseNumber = maxPurchaseNumber
+        
+        tabBarItem.title = productName
+    }
+    
+    
+    // MARK: - Lifecycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        productImageView.image = UIImage(named: productName)
+        
+        stepper.maximumValue = Double(maxPurchaseNumber)
+        stepper.stepValue = Double(increment)
+        
+        setProductCountLabel()
+    }
+    
+    // MARK: - Helpers
+    
+    private func setProductCountLabel() {
+        productCountLabel.text = productName + ": " + "\(productNumber)"
+    }
+    
+    
+    // MARK: - IBAction
+
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        setProductCountLabel()
+    }
+    
+    
+    
+
+}
